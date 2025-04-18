27b5a29cc5869206b4847e9002ef091c753b7a35
dalton <dakribe94@gmail.com>
2025-04-18 06:34:22 -0500

    codelens autocmd

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,4 +1,4 @@
-local diagnostic_icons = require("icons").diagnostics
+local diagnostic_icons = require('icons').diagnostics
 local methods = vim.lsp.protocol.Methods
 
 local M = {}
@@ -15,75 +15,86 @@ local function on_attach(client, bufnr)
 	---@param desc string
 	---@param mode? string|string[]
 	local function keymap(lhs, rhs, desc, mode)
-		mode = mode or "n"
+		mode = mode or 'n'
 		vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
 	end
 
-	keymap("gra", "<cmd>FzfLua lsp_code_actions<cr>", "vim.lsp.buf.code_action()", { "n", "x" })
+	keymap('gra', '<cmd>FzfLua lsp_code_actions<cr>', 'vim.lsp.buf.code_action()', { 'n', 'x' })
 
-	keymap("grr", "<cmd>FzfLua lsp_references<cr>", "vim.lsp.buf.references()")
+	keymap('grr', '<cmd>FzfLua lsp_references<cr>', 'vim.lsp.buf.references()')
 
-	keymap("gy", "<cmd>FzfLua lsp_typedefs<cr>", "Go to type definition")
+	keymap('gy', '<cmd>FzfLua lsp_typedefs<cr>', 'Go to type definition')
 
-	keymap("<leader>vd", vim.diagnostic.open_float, "Open diagnostic float")
+	keymap('<leader>vd', vim.diagnostic.open_float, 'Open diagnostic float')
 
-	keymap("<leader>cc", vim.lsp.codelens.run, "Run codelens")
+	keymap('<leader>cc', vim.lsp.codelens.run, 'Run codelens')
+	keymap('<leader>cC', vim.lsp.codelens.refresh, 'Refresh codelens')
 
-	keymap("<leader>fs", "<cmd>FzfLua lsp_document_symbols<cr>", "Document symbols")
-	keymap("<leader>fS", function()
+	keymap('<leader>fs', '<cmd>FzfLua lsp_document_symbols<cr>', 'Document symbols')
+	keymap('<leader>fS', function()
 		-- Disable the grep switch header.
-		require("fzf-lua").lsp_live_workspace_symbols({ no_header_i = true })
-	end, "Workspace symbols")
+		require('fzf-lua').lsp_live_workspace_symbols({ no_header_i = true })
+	end, 'Workspace symbols')
 
-	keymap("[d", function()
+	keymap('[d', function()
 		vim.diagnostic.jump({ count = -1 })
-	end, "Previous diagnostic")
-	keymap("]d", function()
+	end, 'Previous diagnostic')
+	keymap(']d', function()
 		vim.diagnostic.jump({ count = 1 })
-	end, "Next diagnostic")
-	keymap("[e", function()
+	end, 'Next diagnostic')
+	keymap('[e', function()
 		vim.diagnostic.jump({ count = -1, severity = vim.diagnostic.severity.ERROR })
-	end, "Previous error")
-	keymap("]e", function()
+	end, 'Previous error')
+	keymap(']e', function()
 		vim.diagnostic.jump({ count = 1, severity = vim.diagnostic.severity.ERROR })
-	end, "Next error")
+	end, 'Next error')
 
 	if client:supports_method(methods.textDocument_definition) then
-		keymap("gd", function()
-			require("fzf-lua").lsp_definitions({ jump1 = true })
-		end, "Go to definition")
-		keymap("gD", function()
-			require("fzf-lua").lsp_definitions({ jump1 = false })
-		end, "Peek definition")
+		keymap('gd', function()
+			require('fzf-lua').lsp_definitions({ jump1 = true })
+		end, 'Go to definition')
+		keymap('gD', function()
+			require('fzf-lua').lsp_definitions({ jump1 = false })
+		end, 'Peek definition')
+	end
+
+	if client:supports_method(methods.textDocument_codeLens) then
+		vim.api.nvim_create_autocmd({ 'TextChanged', 'BufEnter', 'CursorHold', 'InsertLeave', 'LspAttach' }, {
+			desc = 'Run codelens',
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.codelens.refresh({ bufnr = bufnr })
+			end,
+		})
 	end
 
 	if client:supports_method(methods.textDocument_signatureHelp) then
-		local blink_window = require("blink.cmp.completion.windows.menu")
-		local blink = require("blink.cmp")
+		local blink_window = require('blink.cmp.completion.windows.menu')
+		local blink = require('blink.cmp')
 
-		keymap("<C-k>", function()
+		keymap('<C-k>', function()
 			-- Close the completion menu first (if open).
 			if blink_window.win:is_open() then
 				blink.hide()
 			end
 
 			vim.lsp.buf.signature_help()
-		end, "Signature help", "i")
+		end, 'Signature help', 'i')
 	end
 
 	if client:supports_method(methods.textDocument_inlayHint) and vim.g.inlay_hints then
-		local inlay_hints_group = vim.api.nvim_create_augroup("toggle_inlay_hints", { clear = false })
+		local inlay_hints_group = vim.api.nvim_create_augroup('toggle_inlay_hints', { clear = false })
 
 		-- Initial inlay hint display.
 		-- Idk why but without the delay inlay hints aren't displayed at the very start.
 		vim.defer_fn(function()
 			local mode = vim.api.nvim_get_mode().mode
-			vim.lsp.inlay_hint.enable(mode == "n" or mode == "v", { bufnr = bufnr })
+			vim.lsp.inlay_hint.enable(mode == 'n' or mode == 'v', { bufnr = bufnr })
 		end, 500)
 
-		vim.api.nvim_create_autocmd("InsertEnter", {
+		vim.api.nvim_create_autocmd('InsertEnter', {
 			group = inlay_hints_group,
-			desc = "Enable inlay hints",
+			desc = 'Enable inlay hints',
 			buffer = bufnr,
 			callback = function()
 				if vim.g.inlay_hints then
@@ -92,9 +103,9 @@ local function on_attach(client, bufnr)
 			end,
 		})
 
-		vim.api.nvim_create_autocmd("InsertLeave", {
+		vim.api.nvim_create_autocmd('InsertLeave', {
 			group = inlay_hints_group,
-			desc = "Disable inlay hints",
+			desc = 'Disable inlay hints',
 			buffer = bufnr,
 			callback = function()
 				if vim.g.inlay_hints then
@@ -107,40 +118,40 @@ end
 
 -- Define the diagnostic signs.
 for severity, icon in pairs(diagnostic_icons) do
-	local hl = "DiagnosticSign" .. severity:sub(1, 1) .. severity:sub(2):lower()
+	local hl = 'DiagnosticSign' .. severity:sub(1, 1) .. severity:sub(2):lower()
 	vim.fn.sign_define(hl, { text = icon, texthl = hl })
 end
 
 -- Diagnostic configuration.
 vim.diagnostic.config({
 	virtual_text = {
-		prefix = "",
+		prefix = '',
 		spacing = 2,
 		format = function(diagnostic)
 			-- Use shorter, nicer names for some sources:
 			local special_sources = {
-				["Lua Diagnostics."] = "lua",
-				["Lua Syntax Check."] = "lua",
+				['Lua Diagnostics.'] = 'lua',
+				['Lua Syntax Check.'] = 'lua',
 			}
 
 			local message = diagnostic_icons[vim.diagnostic.severity[diagnostic.severity]]
 			if diagnostic.source then
-				message = string.format("%s %s", message, special_sources[diagnostic.source] or diagnostic.source)
+				message = string.format('%s %s', message, special_sources[diagnostic.source] or diagnostic.source)
 			end
 			if diagnostic.code then
-				message = string.format("%s[%s]", message, diagnostic.code)
+				message = string.format('%s[%s]', message, diagnostic.code)
 			end
 
-			return message .. " "
+			return message .. ' '
 		end,
 	},
 	float = {
-		source = "if_many",
+		source = 'if_many',
 		-- Show severity icons as prefixes.
 		prefix = function(diag)
 			local level = vim.diagnostic.severity[diag.severity]
-			local prefix = string.format(" %s ", diagnostic_icons[level])
-			return prefix, "Diagnostic" .. level:gsub("^%l", string.upper)
+			local prefix = string.format(' %s ', diagnostic_icons[level])
+			return prefix, 'Diagnostic' .. level:gsub('^%l', string.upper)
 		end,
 	},
 	signs = true,
@@ -165,7 +176,7 @@ local hover = vim.lsp.buf.hover
 ---@diagnostic disable-next-line: duplicate-set-field
 vim.lsp.buf.hover = function()
 	return hover({
-		border = "rounded",
+		border = 'rounded',
 		max_height = math.floor(vim.o.lines * 0.5),
 		max_width = math.floor(vim.o.columns * 0.4),
 	})
@@ -193,8 +204,8 @@ vim.lsp.handlers[methods.client_registerCapability] = function(err, res, ctx)
 	return register_capability(err, res, ctx)
 end
 
-vim.api.nvim_create_autocmd("LspAttach", {
-	desc = "Configure LSP keymaps",
+vim.api.nvim_create_autocmd('LspAttach', {
+	desc = 'Configure LSP keymaps',
 	callback = function(args)
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 
@@ -208,12 +219,12 @@ vim.api.nvim_create_autocmd("LspAttach", {
 })
 
 -- Set up LSP servers.
-vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
+vim.api.nvim_create_autocmd({ 'BufReadPre', 'BufNewFile' }, {
 	once = true,
 	callback = function()
-		local server_configs = vim.iter(vim.api.nvim_get_runtime_file("lsp/*.lua", true))
+		local server_configs = vim.iter(vim.api.nvim_get_runtime_file('lsp/*.lua', true))
 			:map(function(file)
-				return vim.fn.fnamemodify(file, ":t:r")
+				return vim.fn.fnamemodify(file, ':t:r')
 			end)
 			:totable()
 		vim.lsp.enable(server_configs)
